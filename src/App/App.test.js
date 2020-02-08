1cf8ca5d52d2f2a57d173e34498cb17b05c1ed66
Matthew Wagaman <averaqedev@gmail.com>
2020-02-07 16:33:01 -0800

    updated node packages and fixed test suite

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import renderer from 'react-test-renderer';
 
+import { StyleSheetTestUtils } from 'aphrodite';
+
 it('renders without crashing', () => {
   // first create a DOM element to render the component into
   const div = document.createElement('div');
@@ -18,4 +20,12 @@ it('renders the UI as expected', () => {
   const tree = renderer
   .create(<App />).toJSON();
   expect(tree).toMatchSnapshot();  
-  });
+});
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
