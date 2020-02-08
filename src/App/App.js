b4b9e9cd7f94d75fe563395b55fd2c6318342058
Matthew Wagaman <averaqedev@gmail.com>
2020-02-07 16:58:24 -0800

    slight fixes

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { StyleSheet, css } from "aphrodite";
 
 import Header from "../Header/Header";
-import ErrorPage from "../ErrorPage/ErrorPage";
 import SearchBar from "../SearchBar/SearchBar";
 import ResultsList from "../ResultsList/ResultsList";
 import ApiContext from "../ApiContext";
@@ -39,22 +38,8 @@ export default class App extends React.Component {
         <div className={css(styles.app)}>
           <Header />
           <main>
-            <ErrorPage>
               <SearchBar />
-              {this.state.error ? (
-                <div className={css(styles.errorPage)}>
-                  <h2 className={css(styles.errorHeader)}>
-                    Something seems to have gone wrong
-                  </h2>
-                  <p className={css(styles.errorText)}>
-                    Try refreshing the page
-                  </p>
-                </div>
-              ) : (
-                ""
-              )}
               <ResultsList />
-            </ErrorPage>
           </main>
         </div>
       </ApiContext.Provider>
@@ -65,23 +50,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   app: {
     textAlign: "center",
-  },
-
-  errorPage: {
-    backgroundColor: "tomato",
-    width: "75%",
-    margin: "auto auto",
-    padding: "20px",
-    borderRadius: "20px"
-  },
-
-  errorHeader: {
-    color: "red",
-    textDecoration: "underline"
-  },
-
-  errorText: {
-    color: "#bdbdbd",
-    fontSize: "x-large"
   }
 });
